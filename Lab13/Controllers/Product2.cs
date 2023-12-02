9fc196c3d5c40d07b7ba70cc9b079412193fc4e6
Carlos Herrera <carlos.herrera.c@tecsup.edu.pe>
2023-12-01 20:45:17 -0500

    aaa

--- Lab13/Controllers/Product2.cs
+++ Lab13/Controllers/Product2.cs
@@ -1,4 +1,5 @@
 ﻿using Lab13.Models;
+using Lab13.Models.Request;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,6 +22,44 @@ namespace Lab13.Controllers
         {
             var response = _context.Products.Where(x=>x.Price>price).ToList();
             return response;
+        }
+
+        [HttpPost]
+
+        public void Insert(ProductV1 Request)
+        {
+            //trasnformar de request a model
+            Product product = new Product
+            {
+                Price = Request.Price,
+                Name = Request.Name,
+                IsActive =  true
+            };
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+        }
+
+        [HttpPost]
+
+        public void InsertRange(List< ProductV1> Request)
+        {
+            List<Product> products = new List<Product>();
+            foreach (var item in Request) 
+            {
+                Product product = new Product
+                {
+                    Price = item.Price,
+                    Name = item.Name,
+                    IsActive = true
+                };
+
+                _context.Products.Add(product);
+                _context.SaveChanges();
+            }
+
+            _context.Products.AddRange(products);
+            _context.SaveChanges();
         }
     }
 }
---
+++ Lab13/Models/Request/ProductV1.cs
@@ -0,0 +1,8 @@
+﻿namespace Lab13.Models.Request
+{
+    public class ProductV1
+    {
+        public float Price { get; set; }
+        public string Name { get; set; }
+    }
+}
